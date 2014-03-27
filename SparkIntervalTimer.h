90d93a4451d998f3a6ea5df4099fa887cc4bc351
Paul Kourany <pkourany@users.noreply.github.com>
2014-03-27 16:06:30 -0400

    Create SparkIntervalTimer.h

---
+++ SparkIntervalTimer.h
@@ -0,0 +1,93 @@
+/* Copyright (c) 2014 Paul Kourany, based on work by Dianel Gilbert
+
+Copyright (c) 2013 Daniel Gilbert, loglow@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in the
+Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+
+#ifndef __INTERVALTIMER_H__
+#define __INTERVALTIMER_H__
+
+#include "application.h"
+
+extern "C" void TIM2_IRQHandler();
+extern "C" void TIM3_IRQHandler();
+extern "C" void TIM4_IRQHandler();
+
+enum {uSec, hmSec};			// microseconds or half-milliseconds
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+class IntervalTimer {
+  private:
+	typedef void (*ISRcallback)();
+    enum {TIMER_OFF, TIMER_SIT};
+    static const uint8_t NUM_SIT = 3;
+	
+	// Timer ClockDivision = DIV4
+	const uint16_t SIT_PRESCALERu = (uint16_t)(SystemCoreClock / 1000000) - 1;	//To get TIM counter clock = 1MHz
+	const uint16_t SIT_PRESCALERm = (uint16_t)(SystemCoreClock / 2000) - 1;		//To get TIM counter clock = 2KHz
+    const uint16_t MAX_PERIOD = UINT16_MAX;		// 1-65535 us
+	
+    static bool SIT_used[NUM_SIT];
+    bool allocate_SIT(uint16_t newValue, bool scale);
+    void start_SIT(uint16_t newValue, bool scale);
+    void stop_SIT();
+    bool status;
+    uint8_t SIT_id;
+ 	ISRcallback myISRcallback;
+	
+    bool beginCycles(void (*isrCallback)(), uint16_t cycles, bool scale);
+	
+  public:
+    IntervalTimer() { status = TIMER_OFF; }
+    ~IntervalTimer() { end(); }
+	
+    bool begin(void (*isrCallback)(), unsigned int newPeriod, bool scale) {
+		if (newPeriod == 0 || newPeriod > MAX_PERIOD)
+			return false;
+		return beginCycles(isrCallback, newPeriod, scale);
+    }
+	
+    bool begin(void (*isrCallback)(), int newPeriod, bool scale) {
+		if (newPeriod < 0)
+			return false;
+		return begin(isrCallback, (unsigned int)newPeriod, scale);
+    }
+	
+    bool begin(void (*isrCallback)(), unsigned long newPeriod, bool scale) {
+		return begin(isrCallback, (unsigned int)newPeriod, scale);
+    }
+	
+    bool begin(void (*isrCallback)(), long newPeriod, bool scale) {
+		return begin(isrCallback, (int)newPeriod, scale);
+    }
+	
+    void end();
+	
+    static ISRcallback SIT_CALLBACK[NUM_SIT];
+};
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
